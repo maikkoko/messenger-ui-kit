95d6127bbcf96cdcca6ee7d604b9414e12e48b52
maaleman <maaleman@up.edu.ph>
2018-03-27 23:43:02 +0800

    BottomButton support

---
+++ src/components/BottomButton/BottomButton.js
@@ -0,0 +1,28 @@
+/**
+ * @class BottomButton
+ * @prop icon, string - choices: 'arrow', 'upload'. defaults: null
+ * @prop disabled, bool
+ * @prop primary, bool - checks if button has primary styles. default: false
+ * @prop action, function (required) - action taken on button click
+ * @prop text, string. default: 'Submit'
+ */
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Button from '../Button'
+
+import styles from './bottom-button.css'
+
+class BottomButton extends Component {
+  render() {
+    let props = {...this.props, fluid: true, size: "large" }
+
+    return (
+      <div className={styles.bottomButton} >
+        <Button {...props} />
+      </div>
+    )
+  }
+}
+
+export default BottomButton
